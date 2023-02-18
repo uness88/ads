4918956e6f384b7fe9782ed22a342844675220c1
uness88 <113838487+uness88@users.noreply.github.com>
2023-02-18 12:50:41 +0000

    Add files via upload

---
+++ uness1.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Your Google AdSense code
+  var adSenseCode = '<img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://www.aboutcampbtob.eu/wp-content/uploads/2017/01/300x250-example-banner-300x250.jpg">';
+
+  // Append AdSense code to the div with ID "ad-container"
+  $('#ad-container').append(adSenseCode);
+});
